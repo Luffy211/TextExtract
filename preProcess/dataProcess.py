5b1fd4340b6c3fec629f5f658e34a3d0affeffd0
Luffy211 <892442464@qq.com>
2020-06-30 11:37:12 +0800

    P2

--- preProcess/dataProcess.py
+++ preProcess/dataProcess.py
@@ -2,13 +2,15 @@ import pandas as pd
 
 path = '../trainData/sample.csv'
 
+deleteWord = ['厦门市','厦门','思明区','思明','厂','店','摊', '有限公司']
 df = pd.read_csv(path)
 
 with open('../trainData/temp.csv', 'a', encoding='utf-8') as f:
     for i in range(len(df)):
         name = str(df.loc[i]['公司名称'])
-        newName = name.replace('厦门市', '').replace('厦门', '').replace('思明区', '').replace('思明', '').replace('厂', '').replace('店', '').replace('摊', '').replace('有限公司', '')
-        f.write(newName + '\n')
+        for x in deleteWord:
+            name = name.replace(x,'')
+        f.write(name + '\n')
 
 
 
