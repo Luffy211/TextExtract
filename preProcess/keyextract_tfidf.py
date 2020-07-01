abcd76ed6cda113a9e4ac25eb50c4d7424604129
Luffy211 <892442464@qq.com>
2020-07-01 11:42:00 +0800

    P4

--- preProcess/keyextract_tfidf.py
+++ preProcess/keyextract_tfidf.py
@@ -20,7 +20,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 # 数据预处理操作：分词，去停用词，词性筛选
 def dataPrepos(text, stopkey):
     l = []
-    pos = ['n', 'nz', 'v', 'vd', 'vn', 'l', 'a', 'd']  # 定义选取的词性
+    #pos = ['n', 'nz', 'v', 'vd', 'vn', 'l', 'a', 'd']  # 定义选取的词性
+    pos = ['n', 'nz',  'vn',]  # 定义选取的词性
     seg = jieba.posseg.cut(text)  # 分词
     for i in seg:
         if i.word not in stopkey and i.flag in pos:  # 去停用词 + 词性筛选
@@ -50,6 +51,7 @@ def getKeywords_tfidf(data,stopkey,topK):
     weight = tfidf.toarray()
     # 5、打印词语权重
     ids, titles, keys = [], [], []
+    totalWord = []
     #print(word[1],weight[0][1])
     for i in range(len(weight)):
         #print(u"-------这里输出第", i+1 , u"篇文本的词语tf-idf------")
@@ -66,14 +68,28 @@ def getKeywords_tfidf(data,stopkey,topK):
         word_weight = pd.concat([df_word, df_weight], axis=1) # 拼接词汇列表和权重列表
         word_weight = word_weight.sort_values(by="weight",ascending = False) # 按照权重值降序排列
         keyword = np.array(word_weight['word']) # 选择词汇列并转成数组格式
-        print("这里是输出结果 -----")
-        print(keyword)
-        word_split = [keyword[x] for x in range(0,len(df_word))] # 抽取前topK个词汇作为关键词
+        #print("这里是输出结果 -----")
+        #print(keyword)
+        """
+        if len(df_word) >= 10:
+            word_split = [keyword[x] for x in range(0, topK)]  # 抽取前10个词汇作为关键词
+        else:
+            word_split = [keyword[x] for x in range(0, len(df_word))]  # 抽取前topK个词汇作为关键词
+        for word in word_split:
+            totalWord.append(word)"""
+        word_split = [keyword[x] for x in range(0, min(len(df_word),10))]
         word_split = " ".join(word_split)
+        for x in range(0, min(len(df_word), 10)):
+            totalWord.append(keyword[x])
         keys.append(word_split)
-
-    result = pd.DataFrame({"id": ids, "title": titles, "key": keys},columns=['id','title','key'])
-    return result
+        #print(keys)
+    print(len(totalWord))
+    totalWord = list(set(totalWord))
+    print(len(totalWord))
+    #result = pd.DataFrame({"id": ids, "title": titles, "key": keys},columns=['id','title','key'])
+    result = pd.DataFrame({"key": keys},columns=['key'])
+    total = pd.DataFrame({"allword": totalWord},columns=['allword'])
+    return result,total
 
 
 def main():
@@ -83,8 +99,9 @@ def main():
     # 停用词表
     stopkey = [w.strip() for w in codecs.open('../trainData/stopWord.txt', 'r',encoding='utf-8').readlines()]
     # tf-idf关键词抽取
-    result = getKeywords_tfidf(data,stopkey,5)
-    result.to_csv("../trainData/keys_TFIDF.csv",index=False)
+    result,total = getKeywords_tfidf(data,stopkey,10)
+    result.to_csv("../trainData/keys_TFIDF.csv",header=None, index=False)
+    total.to_csv("../trainData/totalWord.csv",header=None, index=False)
 
 if __name__ == '__main__':
     main()
